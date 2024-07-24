f136b7aa8241eac561b08be6b8b9531e17741244
harishragavendar <harishragavendar1202@gmail.com>
2024-07-24 09:11:46 +0530

    login and register ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 function App() {
+  
+  const[users,setUsers]=useState([]);
+
+  const handleRegister=(username,password,email,phone) => {
+    setUsers([...users, {username,password,email,phone}]);
+  }
+
+  const handleLogin=(username,password) => {
+    const user = users.find( 
+      (user) => user.username === username && user.password===password
+    );
+    if(user){
+      alert("Login Success :)");
+    }
+    else{
+      alert("Login Failed! Enter Valid Username or Password");
+    }
+  }
+
+  const[isRegister,setIsRegister] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {isRegister ? <RegisterForm onRegister={handleRegister} /> : <LoginForm onLogin={handleLogin}/>}
+      <br/>
+      <button onClick={()=> setIsRegister(!isRegister)}>
+        {isRegister ? 'Already Registered? Switch to Login here' : 'Not Registered Yet? Switch to Register'}
+      </button>
     </div>
   );
 }
